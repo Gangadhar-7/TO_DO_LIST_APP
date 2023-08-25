31a8ff7df3f1f5b31d2c0bdf74c6d0c7744f3564
Gangadhar_matta <102216404+Gangadhar-7@users.noreply.github.com>
2023-08-25 23:34:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TO_DO_LIST</title>
+    
+
+</head>
+<body>
+    
+    <div id="myDIV" class="header">
+        <h2>My To Do List</h2>
+        <input type="text" id="myInput" placeholder="Title...">
+        <span onclick="newElement()" class="addBtn">Add</span>
+        <link rel="stylesheet" href="style.css">
+        <link rel="script" href="script.js">
+    </div>
+     
+      <ul id="myUL">
+        <li>Hit the gym</li>
+        <li class="checked">attend the Meeting at 6pm</li>
+        <li>go to Market</li>
+        <li>complete the project</li>
+        <li>update the Server settings</li>
+        <li>Organize office</li>
+      </ul>
+    <button>
+
+    </button>
+    
+</body>
+</html>
+
+
